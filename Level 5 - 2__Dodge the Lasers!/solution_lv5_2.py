0f372c649e8b42c9db89906a373357b3bb82823d
FrancescoPaterna <francesco.paterna@studenti.unimi.it>
2024-06-20 14:03:32 +0200

    loading files

--- Level 5 - 2__Dodge the Lasers!/solution_lv5_2.py
+++ Level 5 - 2__Dodge the Lasers!/solution_lv5_2.py
@@ -1,26 +1,14 @@
 from math import floor
-def sum_of_floor_sqrt_2(start, end):
-    result = 0
-    for i in range(start, end + 1):
-        result += math.floor(i * math.sqrt(2))
-        print(math.floor(i * math.sqrt(2)))
-    return int(result)
-
 
 def gauss(n):
-    return n*(n+1)/2
+    return n * (n + 1) / 2
 
 
 def graham(n):
-    a = floor(n**(0.5))
-    b = n*a
-    c = (1.0/3) * (a**3)
-    d = (1.0/2) * (a**2)
-    e = (1.0/6) * (a)
-    print(a,b,c,d,e)
-    return int(b-c-d-e)
+    a = floor(n ** (1 / 2))
+    return int(n * a - (1.0 / 3) * (a ** 3) - (1.0 / 2) * (a ** 2) - (1.0 / 6) * (a))
 
 
 def solution(s):
-    return graham(s)
+    return str(gauss(int(s)) + graham(int(s)))
 
