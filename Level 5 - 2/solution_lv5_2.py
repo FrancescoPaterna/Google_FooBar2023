db799394c3d39880f905b39b8b4f8e55514e0469
FrancescoPaterna <francesco.paterna@studenti.unimi.it>
2024-06-20 13:24:40 +0200

    loading files

--- Level 5 - 2/solution_lv5_2.py
+++ Level 5 - 2/solution_lv5_2.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
 from math import floor
 def sum_of_floor_sqrt_2(start, end):
     result = 0
@@ -13,15 +7,8 @@ def sum_of_floor_sqrt_2(start, end):
     return int(result)
 
 
-
-# In[2]:
-
-
 def gauss(n):
     return n*(n+1)/2
-
-
-# In[3]:
 
 
 def graham(n):
@@ -34,63 +21,6 @@ def graham(n):
     return int(b-c-d-e)
 
 
-# In[4]:
-
-
 def solution(s):
     return graham(s)
-
-
-# In[5]:
-
-
-graham((77))
-
-
-# In[6]:
-
-
-412*(2**0.5)**2
-
-
-# In[7]:
-
-
-77 ** (0.5)
-
-
-# In[8]:
-
-
-77**(0.5)
-
-
-# In[9]:
-
-
-float(1/3)
-
-
-# In[10]:
-
-
-4208-3003
-
-
-# In[11]:
-
-
-(77**2)/2
-
-
-# In[143]:
-
-
-4208-2964
-
-
-# In[ ]:
-
-
-
 
